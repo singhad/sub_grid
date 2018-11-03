fb33d379a99a175f23daaa7c855fb43fe77c5fa9
Amandeep Singh <37579793+singhad@users.noreply.github.com>
2018-11-03 20:22:43 +0100

    updating nomenclature
    changed n to n_H, and n_mean to n_H_mean for more clarity

--- h2_density.py
+++ h2_density.py
@@ -7,12 +7,12 @@ def make_pdf(x, x_mean, s):
     return pdf
 
 #function to calculate Jeans length
-def get_lambda_jeans(T_mean, n):
+def get_lambda_jeans(T_mean, n_H):
     K_b = 1.38064852e-16    # ergs K-1
     m_p = 1.672621777e-24   # g
     G = 6.67408e-8          # dyne cm^2 g^-2
     c_s = np.sqrt(K_b * T_mean / m_p)       # cm s^-1
-    lambda_jeans_cm = c_s / np.sqrt(4* np.pi * G * n * m_p)        # cm
+    lambda_jeans_cm = c_s / np.sqrt(4* np.pi * G * n_H * m_p)        # cm
     #r_jeans_cm = lambda_jeans * 0.5    # cm    #Jeans radius = 0.5*Jeans Length
     return lambda_jeans_cm
 
@@ -21,17 +21,17 @@ if __name__=='__main__':
     m_p = 1.672621777e-24  # g
     T_mean = 10.           #K
     mach_no = 5.
-    n_range = 1000000
-    n_mean = 100    # [H] cm^-3
+    n_H_range = 1000000
+    n_H_mean = 100    # [H] cm^-3
 
     sigma = np.sqrt(np.log(1 + ((0.3 * mach_no)**2))) #sigma in pdf
     x_mean = 1
     pdf = []
-    n = []    # [H] cm^-3
+    n_H = []    # [H] cm^-3
     x = []
     lambda_jeans_cm = []     # cm
     for i in range(1, n_range):
-        n = (np.logspace(-4, 5, 100) * i) # [H] cm^-3
-        x.append(np.log(n/n_mean))
-        pdf.append(make_pdf(np.log(n/n_mean), x_mean, sigma))
-        lambda_jeans_cm.append(get_lambda_jeans(T_mean, n))
+        n_H = (np.logspace(-4, 5, 100) * i) # [H] cm^-3
+        x.append(np.log(n_H/n_H_mean))
+        pdf.append(make_pdf(np.log(n_H/n_H_mean), x_mean, sigma))
+        lambda_jeans_cm.append(get_lambda_jeans(T_mean, n_H))
