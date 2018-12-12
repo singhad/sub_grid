bc22ac25bb29c6b8a20433de2d679b94995308db
Amandeep Singh <37579793+singhad@users.noreply.github.com>
2018-12-12 16:33:33 +0100

    Changes in the pdf_evolution.py
    Changes in the pdf_evolution.py
    Varying mach no and plotting n_H vs pdf
    Varying ratio (t/t_ff) and plotting n_H vs pdf

--- pdf_evolution.py
+++ pdf_evolution.py
@@ -1,4 +1,4 @@
-import numpy as np
+'''import numpy as np
 import matplotlib.pyplot as plt
 import os
 from matplotlib.lines import Line2D
@@ -33,13 +33,13 @@ def get_evolution():
         n_H = np.zeros(1000)
         for i in range(0, 1000):
             s[i] = smin + i*ds
-            n_H[i] = n_H_mean * np.exp(s[i])
-            pdf[i] = make_pdf(s[i], s_bar, sigma_s)
-        plt.plot(s, pdf, color=color)
-    plt.xlabel('s')
-    plt.ylabel('pdf')
+        n_H = n_H_mean * np.exp(s)
+        pdf = make_pdf(s, s_bar, sigma_s)
+        plt.plot(np.log10(n_H), np.log10(pdf), color=color)
+    plt.xlabel('log(n_H)')
+    plt.ylabel('log(pdf)')
     plt.grid(b=True, which='both', axis='both')
-    plt.title('s vs pdf - M=varied, Z=1, G_o=1')
+    plt.title('log(n_H) vs log(pdf) - M=varied, Z=1, G_o=1')
     ax.legend(  custom_lines,
                 [   label + '= 1',
                     label + '= 5',
@@ -47,7 +47,72 @@ def get_evolution():
                     label + '= 50'  ],
                 loc = 'lower right'
                     )
-    plt.savefig(os.path.join('svspdf--M.png'.format()))
+    plt.savefig(os.path.join('log(n_H)vslog(pdf)--M.png'.format()))
+    plt.clf()
+    return s, smin, smax, sigma_s, n_H, pdf
+
+if __name__=='__main__':
+    path = 'for pdf_evolution'
+    os.makedirs(path, exist_ok=True)
+    os.chdir(path)
+
+    # order of variables:
+    # s, smin, smax, sigma_s, n_H, pdf
+    m1, m2, m3, m4, m5, m6 = get_evolution()  #varying mach_no
+
+'''
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+from matplotlib.lines import Line2D
+
+def make_pdf(s, s_bar, sigma_s):
+    pdf = (1/np.sqrt(2*np.pi*(sigma_s**2))) * (np.exp(-0.5*(((s - s_bar)/sigma_s)**2)))
+    return pdf
+
+def get_evolution():
+    fig, ax = plt.subplots()
+    ratio_arr = np.array([0.1, 0.4, 0.7, 1.])
+    label = "(t/t_ff) "
+    color_arr = ['r','g','b','y']
+    custom_lines = [Line2D([0], [0], color='r', lw=4),
+                    Line2D([0], [0], color='g', lw=4),
+                    Line2D([0], [0], color='b', lw=4),
+                    Line2D([0], [0], color='y', lw=4)]
+    for r in range(0, len(ratio_arr)):
+        mach_no = 5
+        ratio = ratio_arr[r]
+        color = str(color_arr[r])
+        sigma_s = np.sqrt(np.log(1 + ((0.3 * mach_no)**2)))
+        s_bar = -0.5*(sigma_s**2)
+        s_tail = 0.2 * s_bar * (ratio**(-2))
+        smin = -4*sigma_s + s_bar - s_tail
+        smax = 4*sigma_s + s_bar + s_tail
+        ds = (smax - smin)/1000
+        value = mach_no
+        n_H_mean = 1e4
+        s = np.zeros(1000)
+        pdf = np.zeros(1000)
+        lambda_jeans = np.zeros(1000)
+        X_H2 = np.zeros(1000)
+        n_H = np.zeros(1000)
+        for i in range(0, 1000):
+            s[i] = smin + i*ds
+        n_H = n_H_mean * np.exp(s)
+        pdf = make_pdf(s, s_bar, sigma_s)
+        plt.plot(np.log10(n_H), pdf, color=color)
+    plt.xlabel('log(n_H)')
+    plt.ylabel('pdf')
+    plt.grid(b=True, which='both', axis='both')
+    plt.title('log(n_H) vs pdf : varying (t/t_ff)')
+    ax.legend(  custom_lines,
+                [   label + '= 0.1',
+                    label + '= 0.4',
+                    label + '= 0.7',
+                    label + '= 1.0'  ],
+                loc = 'lower right'
+                    )
+    plt.savefig(os.path.join('log(n_H)vspdf--ratio.png'.format()))
     plt.clf()
     return s, smin, smax, sigma_s, n_H, pdf
 
