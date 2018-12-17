4fe71d426db6a1965530c513eb574286eb5b2227
Amandeep Singh <37579793+singhad@users.noreply.github.com>
2018-12-17 11:43:21 +0100

    Working code and plot for pdf evolution
    Added the code and plots (working) for pdf evolution with the ratio (t/t_ff), also here the non-log and log plots are in the same figure, plotting subplots in the same figure

--- pdf_evolution.py
+++ pdf_evolution.py
@@ -4,7 +4,7 @@ import os
 from matplotlib.lines import Line2D
 
 def make_pdf(s, s_bar, sigma_s):
-    pdf = (1/np.sqrt(2*np.pi*(sigma_s**2))) * (np.exp(-0.5*(((s - s_bar)/sigma_s)**2))) * ((np.exp(s))**0.5)
+    pdf = (1/np.sqrt(2*np.pi*(sigma_s**2))) * (np.exp(-0.5*(((s - s_bar)/sigma_s)**2))) * np.exp(s)
     return pdf
 
 def get_evolution():
@@ -28,13 +28,14 @@ def get_evolution():
         n_H_mean = 1e4
         s = np.zeros(1000)
         pdf = np.zeros(1000)
+        pdf2 = np.zeros(1000)
         lambda_jeans = np.zeros(1000)
         X_H2 = np.zeros(1000)
         n_H = np.zeros(1000)
         for i in range(0, 1000):
             s[i] = smin + i*ds
-        n_H = n_H_mean * np.exp(s)
-        pdf = make_pdf(s, s_bar, sigma_s)
+            n_H[i] = n_H_mean * np.exp(s[i])
+            pdf[i] = make_pdf(s[i], s_bar, sigma_s)
         plt.plot(np.log10(n_H), pdf, color=color)
     plt.xlabel('log(n_H)')
     plt.ylabel('pdf')
@@ -65,27 +66,32 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 from matplotlib.lines import Line2D
+import matplotlib.gridspec as gridspec
 
 def make_pdf(s, s_bar, sigma_s):
-    pdf = (1/np.sqrt(2*np.pi*(sigma_s**2))) * (np.exp(-0.5*(((s - s_bar)/sigma_s)**2)))
+    pdf = (1/np.sqrt(2*np.pi*(sigma_s**2))) * (np.exp(-0.5*(((s - s_bar)/sigma_s)**2))) #* np.sqrt(np.exp(s)/4)
     return pdf
 
 def get_evolution():
-    fig, ax = plt.subplots()
-    ratio_arr = np.array([0.1, 0.4, 0.7, 1.])
-    label = "(t/t_ff) "
-    color_arr = ['r','g','b','y']
-    custom_lines = [Line2D([0], [0], color='r', lw=4),
-                    Line2D([0], [0], color='g', lw=4),
-                    Line2D([0], [0], color='b', lw=4),
-                    Line2D([0], [0], color='y', lw=4)]
+    fig = plt.figure()
+    gs = gridspec.GridSpec(2, 4)
+    ax1 = plt.subplot(gs[0, :])
+    ax2 = plt.subplot(gs[1, :])
+    G = 6.67408e-8          # dyne cm^2 g^-2
+    m_p = 1.672621777e-24   # g
+    ratio_arr = np.array([0.2, 0.4, 0.6])
+    label = "ratio "
+    color_arr = ['r','g','b']
+    custom_lines = [Line2D([0], [0], color='k', lw=1),
+                    Line2D([0], [0], color='r', lw=1, ls='--'),
+                    Line2D([0], [0], color='g', lw=1, ls='--'),
+                    Line2D([0], [0], color='b', lw=1, ls='--')]
     for r in range(0, len(ratio_arr)):
         mach_no = 5
         ratio = ratio_arr[r]
         color = str(color_arr[r])
         sigma_s = np.sqrt(np.log(1 + ((0.3 * mach_no)**2)))
         s_bar = -0.5*(sigma_s**2)
-        s_tail = np.abs(0.2 * s_bar * (ratio**(-2)))
         smin = -4*sigma_s + s_bar
         smax = 4*sigma_s + s_bar
         ds = (smax - smin)/1000
@@ -93,24 +99,44 @@ def get_evolution():
         n_H_mean = 1e4
         s = np.zeros(1000)
         pdf = np.zeros(1000)
-        lambda_jeans = np.zeros(1000)
-        X_H2 = np.zeros(1000)
         n_H = np.zeros(1000)
+        s_prime = np.zeros(1000)
+        pdf_prime = np.zeros(1000)
+        n_H_prime = np.zeros(1000)
+        t_ff = np.zeros(1000)
+        t_ff_1 = np.zeros(1000)
+        t_ff_2 = 0
+        t = 0
         for i in range(0, 1000):
             s[i] = smin + i*ds
-        n_H = n_H_mean * np.exp(s) * s_tail
+        n_H = n_H_mean * np.exp(s)
+        t_ff_1 = np.sqrt((3 * np.pi)/(32 * G * n_H * m_p))
+        t_ff_2 = np.sqrt((3 * np.pi)/(32 * G * n_H_mean * m_p))
+        t = ratio * t_ff_2
+        n_H_prime = n_H_mean * np.exp(s) * (1/((1-((t/t_ff_1)**2))**2))
+        s_prime = np.log(n_H_prime/n_H_mean)
         pdf = make_pdf(s, s_bar, sigma_s)
-        plt.plot(np.log10(n_H), pdf, color=color)
-    plt.xlabel('log(n_H)')
-    plt.ylabel('pdf')
-    plt.grid(b=True, which='both', axis='both')
-    plt.title('log(n_H) vs pdf : varying (t/t_ff)')
-    ax.legend(  custom_lines,
-                [   label + '= 0.1',
+        pdf_prime = make_pdf(s_prime, s_bar, sigma_s)
+        ax1.plot(np.log10(n_H_prime), pdf, color=color, ls='--')
+        ax2.plot(np.log10(n_H_prime), np.log10(pdf), color=color, ls='--')
+    ax1.plot(np.log10(n_H), pdf, color='k', ls='-')
+    ax2.plot(np.log10(n_H), np.log10(pdf), color='k', ls='-')
+    plt.xlabel('log($n_H$)')
+    ax1.set_ylabel('pdf')
+    ax2.set_ylabel('log(pdf)')
+    ax1.grid(b=True, which='both', axis='both')
+    ax2.grid(b=True, which='both', axis='both')
+    fig.suptitle('log($n_H$) vs pdf and log(pdf): varying ratio')
+    legend = ax1.legend(  custom_lines,
+                [   label + '= 0.0',
+                    label + '= 0.2',
                     label + '= 0.4',
-                    label + '= 0.7',
-                    label + '= 1.0'  ],
-                loc = 'lower right'
+                    label + '= 0.6'  ],
+                loc = 'upper center',
+                bbox_to_anchor = (0.5, 1.21),
+                ncol = 4,
+                fancybox = True,
+                shadow = True
                     )
     plt.savefig(os.path.join('log(n_H)vspdf--ratio.png'.format()))
     plt.clf()
